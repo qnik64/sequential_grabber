d3ad50069fc2cdf888df13ae8c37de722dbb70c0
qnik <piotr-kowa@protonmail.com>
2021-05-14 09:00:00 +0200

    sequential grabbing implemented

--- sequential_grabber.py
+++ sequential_grabber.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
-from urllib.error import URLError
+from urllib.error import HTTPError, URLError
 
 
 def save_file(filename, content):
@@ -14,9 +14,25 @@ def grab_picture(url, name, directory):
         filename = directory + "/" + name
         save_file(filename, url_response.read())
         print("file ", name, " saved.")
+        return True
+    except HTTPError as e:
+        if 404 == e.code:
+            print("file ", name, " not found.")
+            return False
+        print("An HTTP error occurred on ", url, " error type: ", e)
+        return False
     except URLError as e:
         print("An URL error occurred on ", url, " error type: ", e)
+        return False
 
 
-pict_name = 'github-mark.png'
-grab_picture("https://github.githubassets.com/images/modules/open_graph/" + pict_name, pict_name, "./")
+def sequence(start, stop, url_common_part):
+    successes = 0
+    for i in range(start, stop):
+        pict_name = str(i) + ".jpg"
+        if grab_picture(url_common_part + pict_name, pict_name, "./data"):
+            successes += 1
+    print(successes, " files saved ", stop - start - successes, " files skipped")
+
+
+
